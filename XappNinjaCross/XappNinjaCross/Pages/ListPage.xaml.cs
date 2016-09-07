7f78f2047b349a5783422ed5f4cb859250011245
David Zambrano <davazamb10@outlook.com>
2016-09-07 17:58:16 -0400

    Create MainPage, ListPage.  Examples

---
+++ XappNinjaCross/XappNinjaCross/Pages/ListPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XappNinjaCross.Pages
+{
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+            List<String> list = new List<string>();
+            list.Add(string.Empty);
+            list.Add(string.Empty);
+
+            this.lstSample.ItemsSource = list;
+        }
+    }
+}
---
+++ XappNinjaCross/XappNinjaCross/Pages/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XappNinjaCross.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Click(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new ListPage());
+        }
+    }
+}
